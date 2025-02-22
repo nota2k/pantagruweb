f818bf092d8a165b7c29a34a0f3c12e9162e3be7
Nelly <nelly.babillon@gmail.com>
2025-02-22 22:49:48 +0100

    Mobile v2

---
+++ config/custom.js
@@ -0,0 +1,23 @@
+console.log('custom.js loaded');
+     const servicesDiv = document.querySelectorAll('.services-group');
+     console.log('Nombre de .services-group trouvés :', servicesDiv.length);
+
+     servicesDiv.forEach(function (div) {
+          const newElement = document.createElement('div');
+          newElement.classList.add('resume');
+          newElement.innerHTML = '<h2>Titre</h2><p>Service description</p>';
+          div.appendChild(newElement);
+     });
+
+     let descriptions = document.querySelectorAll('.resume p');
+     let title = document.querySelectorAll('.resume h2');
+
+     if (title.length > 0) {
+          title[0].textContent = " MIOUM MIOUM MIOUM";
+          descriptions[0].textContent = "Pantagruweb a bien mangé et se sent un peu ballonné, mais cela ne le gêne pas, au contraire : il est convaincu que l'accumulation de gaz dans son estomac finira par lui permettre de s'envoler, telle une bulle de savon. En attendant, il a fait le serment de conserver, de valoriser et de rendre accessible au plus grand nombre les exquis morceaux de web dont il s'est régalé.";
+     }
+
+     if (title.length > 1) {
+          title[1].textContent = " Pantagruweb a faim";
+          descriptions[1].textContent = "Pris d'une fringale impitoyable et vieille comme le siècle, Pantagruweb dévore internet avec paresse et minutie.Son ventre - à - bière, ce poisson - globe aérobique, gonfle, mais jamais n’éclate.Cet abdomen distendu, nous l'appellerons Unser Bierbauch, Car nous sommes Pantagruweb.";
+     }
