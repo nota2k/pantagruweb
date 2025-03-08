983936917f4e94ca91090ddd3261ca565a915035
Nelly <nelly.babillon@gmail.com>
2025-03-08 18:38:08 +0100

    Mise à jour de la configuration et des services : activation des hauteurs égales, suppression de sections obsolètes et ajustement du script personnalisé pour améliorer l'affichage des services.

--- config/custom.js
+++ config/custom.js
@@ -1,6 +1,7 @@
 console.log('custom.js loaded');
      const servicesDiv = document.querySelectorAll('.services-group');
-     for(let i = 1; i < servicesDiv.length; i++) {
+
+     for(let i = 0; i < servicesDiv.length; i++) {
           const newElement = document.createElement('div');
           newElement.classList.add('resume');
           newElement.innerHTML = '<h2>Titre</h2><p>Service description</p>';
@@ -9,6 +10,7 @@ console.log('custom.js loaded');
 
      let descriptions = document.querySelectorAll('.resume p');
      let title = document.querySelectorAll('.resume h2');
+     console.log(descriptions);
 
      if (title.length > 0) {
           title[0].textContent = " MIOUM MIOUM MIOUM";
@@ -18,4 +20,10 @@ console.log('custom.js loaded');
      if (title.length > 1) {
           title[1].textContent = " Pantagruweb a faim";
           descriptions[1].textContent = "Pris d'une fringale impitoyable et vieille comme le siècle, Pantagruweb dévore internet avec paresse et minutie.Son ventre - à - bière, ce poisson - globe aérobique, gonfle, mais jamais n’éclate.Cet abdomen distendu, nous l'appellerons Unser Bierbauch, Car nous sommes Pantagruweb.";
+
+          const newImg = document.createElement('div');
+          newImg.classList.add('service-img');
+          descriptions[1].prepend(newImg);
      }
+
+    
